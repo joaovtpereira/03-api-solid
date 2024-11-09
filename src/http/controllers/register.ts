6c1a34297475adb2f1c45e55aaf73372bd124995
joaovtpereira <joaovitorpessoal2@gmail.com>
2024-11-09 17:39:14 -0300

    add bcryptjs to generate hash

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
@@ -8,11 +9,22 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     password: z.string().min(6),
   })
   const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
   return reply.status(201).send()
