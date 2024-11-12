52d4c25eced2d0ca736e3b262b91758df4d2b528
joaovtpereira <joaovitorpessoal2@gmail.com>
2024-11-12 19:39:36 -0300

    add usecases and tests

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -2,6 +2,11 @@ import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    console.log({ id })
+    return null
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
